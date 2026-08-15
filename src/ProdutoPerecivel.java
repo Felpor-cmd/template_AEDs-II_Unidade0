b47793b58b7cfaf46660212f76f7cd1d44ee9759
Felpor-cmd <filipejosebragadias@gmail.com>
2026-08-15 20:46:44 -0300

    Atv1

--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -8,19 +8,28 @@ public class ProdutoPerecivel extends Produto {
     public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate dataValidade){
         super(desc, precoCusto, margemLucro);
 
-        if dataValidade.isAfter(LocalDate.now()) {
+        if (dataValidade.isAfter(LocalDate.now())) {
             this.dataValidade = dataValidade;
         } else {
             throw new IllegalArgumentException("Data de validade inválida.");
         }
     }
 
-    public valorVenda(){
-        if (dataValidade.)
+    public double valorVenda(){
+        double valorVenda = 0;
+        if (dataValidade.isBefore(LocalDate.now()))
+            throw new IllegalArgumentException("Produto vencido.");
+        
+        if (dataValidade.isBefore(LocalDate.now().plusDays(PRAZO_DESCONTO))) {
+            valorVenda = super.valorDeVenda() * (DESCONTO);
+        } else {
+            valorVenda = super.valorDeVenda();
+        }
+
+        return valorVenda;
     }
 
-    @Override
     public String toString(){
-
+        return super.toString() + " - Validade: " + dataValidade;
     }
 }
